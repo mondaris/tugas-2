f4dd2a40ddd07effc23f289c5403478e0ae27e64
mondaris <mohammadnafiis.19@gmail.com>
2023-09-20 21:41:50 +0700

    mantap tugas 2

--- lib/ui/form_data.dart
+++ lib/ui/form_data.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:tugas-2/ui/tampil_data.dart';
 
 class FormData extends StatefulWidget {
   const FormData({Key? key}) : super(key: key);
@@ -9,12 +10,66 @@ class FormData extends StatefulWidget {
 }
 
 class _FormDataState extends State<FormData> {
+//MULAI
+  final _namaTextboxController = TextEditingController();
+  final _nimTextboxController = TextEditingController();
+  final _thnLahirTextboxController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Input Data"),
-      ),
-    );
+        appBar: AppBar(
+          //
+          title: Text("Input Data"),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: "Nama"),
+                controller: _namaTextboxController,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: "NIM"),
+                controller: _nimTextboxController,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: "Tahun Lahir"),
+                controller: _thnLahirTextboxController,
+              ),
+
+              //menambahkan margin
+              Container(
+                  margin: const EdgeInsets.only(top: 8.0),
+                  child: ElevatedButton(
+                    child: Text('Simpan'),
+                    onPressed: () {
+                      String nama =
+                          _namaTextboxController.text; //untuk menampung nama
+                      String nim = _nimTextboxController.text;
+                      int thnLahir = int.parse(
+                          _thnLahirTextboxController.text); //parsing data
+
+                      //ketika di klik akan pindah ke halaman tampil
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => TampilData(
+                                nama: nama, //nama yang telah diinputkan
+                                nim: nim,
+                                thnLahir: thnLahir,
+                              )));
+                    },
+                  ))
+            ],
+          ),
+        ));
   }
+}
+
+//hitung umur
+int hitungUmur(int thnLahir) {
+  DateTime sekarang = DateTime.now(); //tahun saat ini
+  int thnSekarang = sekarang.year;
+  int selisihthn = thnSekarang - thnLahir; //selisih tahun
+  return selisihthn; //mengembalikan ke nilai umur sekarang
 }
--- lib/ui/tampil_data.dart
+++ lib/ui/tampil_data.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:tugas-2/ui/form_data.dart';
 
 class TampilData extends StatelessWidget {
-  final String data; //contoh data yang di-passing
-  
-  const TampilData({Key? key, required this.data}) : super(key: key);
+  final String nama; //data yang dipassing
+  final String nim; //variabel nim
+  final int thnLahir;
+
+  const TampilData(
+      {Key? key, required this.nama, required this.nim, required this.thnLahir})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Perkenalan"),
+      ),
+      body: Column(
+        children: [
+          Text("Nama saya $nama"
+              ", NIM $nim"
+              ", dan umur saya adalah ${hitungUmur(thnLahir)} tahun"),
+        ],
       ),
     );
   }
